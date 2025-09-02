8a59d6c794577ddadfa42e8fd3172f46822f7df0
李鸿耀 <lihy_online@163.com>
2025-09-02 17:07:44 +0800

    fix: 新增私有化部署打包环境变量配置

---
+++ config/config.private.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  // 1. 部署至二级目录
+  // base: '/二级目录名/',
+  // publicPath: '/二级目录名/',
+
+  // 2. 输出目录
+  outputPath: 'dist',
+
+  // 3. 定义环境变量
+  define: {
+    'process.env.BASE': '',
+    'process.env.NAME': 'production',
+    'process.env.HOST': '此处为私有化部署服务器地址',
+    'process.env.SECRET_KEY': '盐值',
+  },
+});
